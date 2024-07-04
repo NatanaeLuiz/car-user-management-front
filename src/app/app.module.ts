46aa01c15e709574911300b918dc43f8e399aa96
Natanael Rolim <natanael.10@hotmail.com>
2024-07-04 00:55:46 -0300

    Add Angular Material project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Componentes Terceiros
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Componentes do Projeto
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
